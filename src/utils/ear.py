f048af36787b4fe07038ea349848bf51af7023fb
Shaun D Joseph <shaundjoseph@gmail.com>
2026-09-03 22:36:04 +0530

    Replace scipy import with built-in math.dist in ear.py

--- src/utils/ear.py
+++ src/utils/ear.py
@@ -1,14 +1,15 @@
-from scipy.spatial import distance
+import math
 
 def calculate_ear(eye_points):
-
-    # Vertical distances
-    vertical_1 = distance.euclidean(eye_points[1], eye_points[5])
-
-    vertical_2 = distance.euclidean(eye_points[2], eye_points[4])
+    # Vertical distances using built-in math.dist
+    vertical_1 = math.dist(eye_points[1], eye_points[5])
+    vertical_2 = math.dist(eye_points[2], eye_points[4])
 
     # Horizontal distance
-    horizontal = distance.euclidean(eye_points[0], eye_points[3])
+    horizontal = math.dist(eye_points[0], eye_points[3])
+
+    if horizontal == 0:
+        return 0.0
 
     # EAR formula
     ear = (vertical_1 + vertical_2) / (2.0 * horizontal)
